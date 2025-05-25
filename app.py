975294502ab297f7306a8fd669c27de3c7d0e0d6
William1030Hsieh <william1030Heish>
2025-05-25 20:40:58 +0800

    trigger redeploy

--- app.py
+++ app.py
@@ -97,4 +97,6 @@ def detect_pills(image_path):
     return result_text, "annotated_pills.jpg"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+     port = int(os.environ.get('PORT', 10000))
+     app.run(host='0.0.0.0', port=port)
+    #app.run(host="0.0.0.0", port=10000)
